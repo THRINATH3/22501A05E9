2a081606b36a8bfd7c6337d7c74410daa479323a
THRINATH3 <153103919+THRINATH3@users.noreply.github.com>
2025-06-28 11:28:02 +0530

    added the funtionality of form submission

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,14 +7,21 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const {handleSubmit,register} = useForm();
+  function getURL(data){
+     console.log(data);
+  }
   return (
     <>
-      <div className="container bg-secondary" style={{minHeight:'50vh',borderRadius:'10px',alignContent:'center'}}>
+      <div className="container bg-secondary" style={{minHeight:'50vh',borderRadius:'10px',alignContent:'center',width:'500px',padding:'50px'}}>
         <h2 className='text-white'>Long URL's to Short URL's</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit(getURL)}>
           <div className="">
-          <label htmlFor="url" className='form-label text-white'>Enter the url:</label>
-          <input type="url" className='form-control' name='url' id='url'/>
+          <label htmlFor="urlw" className='form-label text-white'>Enter the url:</label>
+          <input type="url" className='form-control' name='urlw' id='urlw'
+          {...register('urlw')}/>
+          </div>
+          <div className="text-center mt-5">
+              <button className="btn btn-warning" type='submit'>Submit</button>
           </div>
         </form>
       </div>
